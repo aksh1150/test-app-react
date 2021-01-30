651b4060b03c97cf7d6e3305d86268eb2513a9ca
Akshit Patel <aksh1150@gmail.com>
2021-01-30 09:14:25 -0500

    Add hooks

--- src/components/Practitioner.js
+++ src/components/Practitioner.js
@@ -1,14 +1,18 @@
 import React, { Component, useState, useEffect } from "react";
 import { getPractitioners } from "../services";
 
-const Practitioner = () => {
-  const [practitioners, setPractitioners] = useState([]);
+class Practitioner extends Component {
+  state = {
+    practitioners: [],
+  };
 
-  // componentDidMount() {
+  componentDidMount() {
+    getPractitioners().then((res) => {
+      this.setState({ practitioners: this.flattenPractitionerObj(res) });
+    });
+  }
 
-  // }
-
-  const flattenPractitionerObj = (response) => {
+  flattenPractitionerObj = (response) => {
     return (response.data.entry || []).map((item) => {
       const name = item.resource.name || [];
       return {
@@ -24,44 +28,37 @@ const Practitioner = () => {
     });
   };
 
-  useEffect(() => {
-    getPractitioners().then((res) => {
-      // this.setState({ practitioners: this.flattenPractitionerObj(res) });
-      setPractitioners({ practitioners: flattenPractitionerObj(res) });
-    });
-  });
-
-  // render() {
-  // const { practitioners } = this.state;
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Profile Image</th>
-          <th>Full Name</th>
-          <th>Gender</th>
-          <th>Date of Birth</th>
-        </tr>
-      </thead>
-      <tbody>
-        {practitioners.map((practitioner) => (
-          <tr key={practitioner.id}>
-            <td>
-              <img
-                src={practitioner.photo}
-                alt="Avatar"
-                style={{ height: 50, width: 50, borderRadius: "50%" }}
-              />
-            </td>
-            <td>{practitioner.name}</td>
-            <td>{practitioner.gender}</td>
-            <td>{practitioner.dob}</td>
+  render() {
+    const { practitioners } = this.state;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Profile Image</th>
+            <th>Full Name</th>
+            <th>Gender</th>
+            <th>Date of Birth</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
-// }
+        </thead>
+        <tbody>
+          {practitioners.map((practitioner) => (
+            <tr key={practitioner.id}>
+              <td>
+                <img
+                  src={practitioner.photo}
+                  alt="Avatar"
+                  style={{ height: 50, width: 50, borderRadius: "50%" }}
+                />
+              </td>
+              <td>{practitioner.name}</td>
+              <td>{practitioner.gender}</td>
+              <td>{practitioner.dob}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default Practitioner;
