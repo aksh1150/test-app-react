598c69806fba55802f494ef8a9ebdda1a0cc379c
Akshit Patel <aksh1150@gmail.com>
2021-01-30 16:56:40 -0500

    Add dialog for confirmation

--- src/components/Practitioner/Practitioner.js
+++ src/components/Practitioner/Practitioner.js
@@ -3,9 +3,13 @@ import { getPractitioners } from "../../services";
 
 import PractitionerCard from "../PractitionerCard/PractitionerCard";
 import DateFormat from "../../utils/dateFormate";
+import "./Practitioner.css";
+
 const Practitioner = () => {
   const [practitioners, setPractitioners] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [deleteId, setDeleteId] = useState(null);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -36,34 +40,48 @@ const Practitioner = () => {
 
   const deletePractitioners = async (id) => {
     // console.log("practitioners", practitioners);
+    setDeleteId(id);
+    setIsConfirmDialogOpen(true);
+  };
+
+  const confirmDelete = async () => {
     const newPractitionerObject = await practitioners.filter(
-      (e) => e.id !== id
+      (e) => e.id !== deleteId
     );
     setPractitioners(newPractitionerObject);
     // console.log("delete practitioners", practitioners);
   };
 
   return (
-    <div className="row">
-      {loading ? (
-        <PractitionerCard loading="Loading.." />
-      ) : (
-        practitioners.map((practitioner) => (
-          <PractitionerCard
-            key={practitioner.id}
-            src={practitioner.photo}
-            name={
-              practitioner.name.includes("undefined")
-                ? "N/A"
-                : practitioner.name
-            }
-            gender={practitioner.gender}
-            dob={practitioner.dob ? DateFormat(practitioner.dob) : ""}
-            handleDelete={() => deletePractitioners(practitioner.id)}
-          />
-        ))
-      )}
-    </div>
+    <>
+      <div className="confirmModel">
+        <h3>Are you sure, you want to delete this record?</h3>
+        <button className="btn-no">No</button>
+        <button onClick={confirmDelete} className="btn-delete">
+          Yes
+        </button>
+      </div>
+      <div className="row">
+        {loading ? (
+          <PractitionerCard loading="Loading..." />
+        ) : (
+          practitioners.map((practitioner) => (
+            <PractitionerCard
+              key={practitioner.id}
+              src={practitioner.photo}
+              name={
+                practitioner.name.includes("undefined")
+                  ? "N/A"
+                  : practitioner.name
+              }
+              gender={practitioner.gender}
+              dob={practitioner.dob ? DateFormat(practitioner.dob) : ""}
+              handleDelete={() => deletePractitioners(practitioner.id)}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
