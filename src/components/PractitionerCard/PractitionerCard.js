f26f96879017321ac365ef203665bb1057447370
Akshit Patel <aksh1150@gmail.com>
2021-01-30 09:53:58 -0500

    Create simple HTML for card layout

--- src/components/PractitionerCard/PractitionerCard.js
+++ src/components/PractitionerCard/PractitionerCard.js
@@ -2,7 +2,34 @@ import React, { useState, useEffect } from "react";
 import "./PractitionerCard.css";
 
 const PractitionerCard = () => {
-  return <p>Pract</p>;
+  return (
+    <div className="row">
+      <div className="column">
+        <div className="card">
+          <h2>title</h2>
+          <p>content</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <h2>title</h2>
+          <p>content</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <h2>title</h2>
+          <p>content</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <h2>title</h2>
+          <p>content</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PractitionerCard;
