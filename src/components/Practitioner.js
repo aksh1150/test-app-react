b33e18f08d84b465cc0238a43053977cbf1f56ac
Akshit Patel <aksh1150@gmail.com>
2021-01-30 09:06:00 -0500

    Remove render

--- src/components/Practitioner.js
+++ src/components/Practitioner.js
@@ -2,18 +2,16 @@ import React, { Component, useState, useEffect } from "react";
 import { getPractitioners } from "../services";
 
 const Practitioner = () => {
- 
-
   const [practitioners, setPractitioners] = useState([]);
 
   useEffect(() => {
- getPractitioners().then((res) => {
+    getPractitioners().then((res) => {
       this.setState({ practitioners: this.flattenPractitionerObj(res) });
     });
-  })
+  });
 
   // componentDidMount() {
-   
+
   // }
 
   flattenPractitionerObj = (response) => {
@@ -32,37 +30,37 @@ const Practitioner = () => {
     });
   };
 
-  render() {
-    // const { practitioners } = this.state;
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Profile Image</th>
-            <th>Full Name</th>
-            <th>Gender</th>
-            <th>Date of Birth</th>
+  // render() {
+  // const { practitioners } = this.state;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Profile Image</th>
+          <th>Full Name</th>
+          <th>Gender</th>
+          <th>Date of Birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        {practitioners.map((practitioner) => (
+          <tr key={practitioner.id}>
+            <td>
+              <img
+                src={practitioner.photo}
+                alt="Avatar"
+                style={{ height: 50, width: 50, borderRadius: "50%" }}
+              />
+            </td>
+            <td>{practitioner.name}</td>
+            <td>{practitioner.gender}</td>
+            <td>{practitioner.dob}</td>
           </tr>
-        </thead>
-        <tbody>
-          {practitioners.map((practitioner) => (
-            <tr key={practitioner.id}>
-              <td>
-                <img
-                  src={practitioner.photo}
-                  alt="Avatar"
-                  style={{ height: 50, width: 50, borderRadius: "50%" }}
-                />
-              </td>
-              <td>{practitioner.name}</td>
-              <td>{practitioner.gender}</td>
-              <td>{practitioner.dob}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+        ))}
+      </tbody>
+    </table>
+  );
+};
+// }
 
 export default Practitioner;
